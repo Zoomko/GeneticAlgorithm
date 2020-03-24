a0e3383f8763e848fc580552532d0f0161a2a61f
Michael <zagajnovmikhail@gmail.com>
2020-03-25 02:37:32 +0400

    Object reset was seccessful

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature:MonoBehaviour
+{
+    float score = 0f;
+    bool isAlive = true;
+    HingeJoint[] hinges;
+    List<(Transform,MyTransform)> PartsOfBody;    
+    private void Awake()
+    {                
+        hinges = GetComponentsInChildren<HingeJoint>();
+        PartsOfBody = new List<(Transform, MyTransform)>();
+        foreach (var i in GetComponentsInChildren<Transform>())
+        {
+            Transform t = i as Transform;
+            if (i != null)
+            {
+                PartsOfBody.Add((t, new MyTransform(t.position, t.rotation)));                
+            }
+        }
+        print(PartsOfBody.Count);
+    }
+   
+    private void Update()
+    {
+        foreach(var i in hinges)
+        {
+            i.motor = ChangetargetVelocity(i.motor);
+        }
+    }
+    public void ResetPosition()
+    {
+        foreach (var it in PartsOfBody)
+        {
+            it.Item1.position = it.Item2.Position;
+            it.Item1.rotation = it.Item2.Rotaion;
+        }
+    }
+    JointMotor ChangetargetVelocity(JointMotor m)
+    {
+        var motor = m;
+        if (Random.value > 0.8)
+        {
+            motor.targetVelocity = 100;
+        }
+        else
+        {
+            motor.targetVelocity = -100;
+        }
+        return motor;
+    }    
+}
+class MyTransform
+{
+    float x;
+    float y;
+    float z;
+    Quaternion rotation;
+    public MyTransform(Vector3 v, Quaternion q)
+    {
+        x = v.x;
+        y = v.y;
+        z = v.z;
+        rotation = q;
+    }
+    public Vector3 Position
+    {
+        get
+        {
+            return new Vector3(x, y, z);
+        }
+    }
+    public Quaternion Rotaion
+    {
+        get
+        {
+            return rotation;
+        }
+    }
+}
---
+++ Assets/Scripts/Population.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Population : MonoBehaviour
+{
+    public int size;
+    public int generationNumber;    
+    public GameObject creature;
+    List<GameObject> creatures;
+
+    private void Awake()
+    {
+        creatures = new List<GameObject>();
+        for (int i = 0; i < size;i++)
+        {
+            creatures.Add(Instantiate(creature, new Vector3(i * 10, 10f, 0), Quaternion.identity));
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach(var i in creatures)
+            {
+                i.GetComponent<Creature>().ResetPosition();
+            }
+        }
+    }
+}
